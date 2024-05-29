6ad38cf879eb84694e63d42611db0e3d41cafa78
GregMac <gregorypotesgonzalez@gmail.com>
2024-05-29 22:35:37 +0200

    Added Arc for Device

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,19 @@
-use base64::engine::general_purpose::STANDARD as base64Encoding;
-use base64::Engine;
 use std::error::Error;
+use std::sync::Arc;
+
+use actix_web::{App, get, HttpResponse, HttpServer, Responder};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-use wireguard::wireguard::StubTun;
+use wiretun::{Cidr, Device, DeviceConfig, PeerConfig, Tun};
 
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use wiretun::{uapi, Cidr, Device, DeviceConfig, PeerConfig, Tun, TunError};
+mod tunneling;
+use tunneling::wireguard::StubTun;
+mod utils;
+use utils::utils::local_private_key;
+use crate::utils::utils::peer_public_key;
 
-mod wireguard;
-
-fn decode_base64(s: &str) -> Vec<u8> {
-    base64Encoding.decode(s).unwrap()
-}
-
-fn local_private_key() -> [u8; 32] {
-    decode_base64("cCt9aay9r1qflp0OseQQkQ19Zjayx3M3tW9MRqV4aHc=")
-        .try_into()
-        .unwrap()
-}
-
-fn peer_public_key() -> [u8; 32] {
-    decode_base64("t2Vc/46ESybZDtMqGZNAPNq2+I9XMFeLZItTxSWvHlU=")
-        .try_into()
-        .unwrap()
-}
 
 #[actix_web::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let app = HttpServer::new(|| App::new().service(hello))
-        .bind(("127.0.0.1", 8080))?
-        .run();
-
+async fn main() -> std::io::Result<()> {
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::new(
             std::env::var("RUST_LOG").unwrap_or_else(|_| "debug".into()),
@@ -38,25 +21,27 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
+    // Créez le device ici
     let cfg = DeviceConfig::default()
         .listen_port(51820)
         .private_key(local_private_key())
         .peer(
             PeerConfig::default()
                 .public_key(peer_public_key())
-                .allowed_ip("10.0.0.1".parse::<Cidr>()?),
+                .allowed_ip("10.0.0.1".parse::<Cidr>().unwrap()),
         );
     let tun = StubTun::new();
-    let device = Device::with_udp(tun, cfg).await.unwrap();
+    let device = Arc::new(Device::with_udp(tun, cfg).await.unwrap());
 
-    let ctrl = device.control();
-    tokio::spawn(uapi::bind_and_handle(ctrl));
-
-    HttpServer::new(move || App::new().service(hello))
+    // Définissez les routes Actix Web ici
+    HttpServer::new(move || {
+        App::new()
+            .data(device.clone())
+            .service(hello)
+    })
         .bind(("127.0.0.1", 8080))?
         .run()
-        .await;
-    Ok(())
+        .await
 }
 
 #[get("/")]
---
+++ src/utils.rs
@@ -0,0 +1,20 @@
+pub mod utils {
+    use base64::engine::general_purpose::STANDARD as base64Encoding;
+    use base64::Engine;
+
+    pub fn decode_base64(s: &str) -> Vec<u8> {
+        base64Encoding.decode(s).unwrap()
+    }
+
+    pub fn local_private_key() -> [u8; 32] {
+        decode_base64("cCt9aay9r1qflp0OseQQkQ19Zjayx3M3tW9MRqV4aHc=")
+            .try_into()
+            .unwrap()
+    }
+
+    pub fn peer_public_key() -> [u8; 32] {
+        decode_base64("t2Vc/46ESybZDtMqGZNAPNq2+I9XMFeLZItTxSWvHlU=")
+            .try_into()
+            .unwrap()
+    }
+}
